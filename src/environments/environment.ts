e6d60668280f45f830de71181b12c05ceef72fb6
aleksandarstankovic <stankovic.aleksandar@elfak.rs>
2019-08-30 16:20:46 +0200

    environments for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
     '997850375082-ah4oortksj8mel1ighp41d8q0fjhj9bf.apps.googleusercontent.com',
   GOOGLE_AUTH_SECRET: 'qaEVcb46y37D7NqXKm4m5fWm',
   GOOGLE_API_KEY: 'AIzaSyB2hCU1WfEy1m92PMfFtGcIGEYeB5D_Lfs',
+  API_ENDPOINT: 'http://sheets.xploit.tech/api',
   production: false,
-  API_ENDPOINT: 'http://localhost:3010/api',
 }
 
 /*
